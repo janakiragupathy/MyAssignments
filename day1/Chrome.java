638bfc0a09a1d017a1ee72e850adabc32d72532b
janakiragupathy <janaki.ragupathy@gmail.com>
2024-08-13 04:16:45 -0400

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("version: "+ version);
+		System.out.println("developer: "+ developer+'\n'+"isBeta: "+isBeta+'\n'+"releaseYear: "+releaseYear+'\n'+
+				"shortcutKey: "+ shortcutKey);
+				
+	}
+
+}
